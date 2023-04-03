9c6c0e49099aeee459e93f394cc2dede1da53b8a
Jéssica Almeida de Oliveira <32886156+jessicaalmeidadeoliveiradede@users.noreply.github.com>
2023-04-03 20:29:50 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@cenario-3/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+struct list{
+int numero;
+ 
+};
+int main(void) {
+  void list(numero){
+    adiciona(6);
+    printf("adiciona o numero");
+  };
+    void imprime(listvazia);{
+       imprime(0);
+      printf("imprime uma fazialista fazia",list);
+  };
+    void remove(list){
+      list(6);
+     printf("lista fazia\n",remove);
+      
+    };
+
+  return 0;
+}
